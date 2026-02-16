62071c6a04bbcf771fe8ece9333df565c76bfdc2
Kevin de la Cruz Sánchez <152937407+cruzada07@users.noreply.github.com>
2026-02-16 17:52:04 -0500

    Corrigiendo problema 1

--- 4taPracticaCalificada/Ejercicio1.cpp
+++ 4taPracticaCalificada/Ejercicio1.cpp
@@ -1,39 +1,79 @@
 //Autor: Kevin de la Cruz Sanchez
 #include <iostream>
 using namespace std;
+
 void sumarPolinomios(int* coef1,int* coef2, int* coef3, int g1, int g2, int g3){
-    int gradMayor=0;
-    if(g1>=g2 && g1>=g3){
-        gradMayor=g1;
-    }
-    if(g2>=g1 && g2>=g3){
+    int gradMayor=g1;
+    if(g2>gradMayor){
         gradMayor=g2;
     }
-    if(g3>=g1 && g3>=g2){
+    if(g3>gradMayor){
         gradMayor=g3;
     }
-    int* coefsuma= new int [gradMayor+1];
+
+    int* coefsuma = new int[gradMayor+1];
     
-    coefsuma[0]=coef1[0]+0+coef3[0];
-    coefsuma[1]=coef1[1]+coef2[0]+coef3[1];
-    coefsuma[2]=coef1[2]+coef2[1]+coef3[2];
-    for(int i=0;i<gradMayor+1;i++){
-        cout<<"+("<<coefsuma[i]<<")x^"<<gradMayor-i;
+    for(int i=gradMayor;i>= 0;i--){
+        int sumaTermino=0;
+        if(i<= g1){
+            sumaTermino += coef1[g1-i];
+        }
+        if(i<= g2){
+            sumaTermino += coef2[g2-i];
+        }
+        if(i<= g3){
+            sumaTermino += coef3[g3-i];
+        }
+        
+        coefsuma[gradMayor-i] =sumaTermino;
+
     }
+
+    cout<<"Suma de los tres polinomios: ";
+    for(int i=0;i<=gradMayor;i++){
+        if(i>0   &&  coefsuma[i] >=0){
+            cout<<" + ";
+
+        }
+        cout<<coefsuma[i]<<"x^"<<gradMayor-i;
+
+    }
+    cout<<"\n";
     delete[] coefsuma;
 }
 
-void multiplicarPolinomios(int* coef1,int* coef2, int* coef3, int g1, int g2, int g3){
-    int* coefmult = new int[(g1+g2+g3)+1];
+void multiplicarPolinomios(int*  coef1,int* coef2,int* coef3, int g1,int g2,int g3){
+    int gradoFinal = g1+g2+g3;
+    int* coefmult = new int[gradoFinal+1];
+    
+    for (int i=0;i<=gradoFinal;i++){
+        coefmult[i] = 0;
+    }
 
+    for(int i=0;i<=g1;i++){
+        for(int j=0;j<=g2;j++){
+            for(int k=0;k<=g3;k++){
+                coefmult[i+j+k] += coef1[i] * coef2[j] * coef3[k];
+            }
 
+        }
+    }   
 
+    cout<<"Producto de los tres polinomios: ";
+    for(int i=0;i<=gradoFinal;i++){
+        if(i>0  &&   coefmult[i]>=0){
+            cout<<" + ";
 
+        }
+        cout<<coefmult[i]<<"x^"<<gradoFinal-i;
 
-
-
+    }
+    cout<<"\n";
     delete[] coefmult;
+
+
 }
+
 
 int main(){
     int g1=2,g2=1,g3=2;
@@ -52,34 +92,44 @@ int main(){
     coef3[0]=3;
     coef3[1]=0;
     coef3[2]=1;
-    //mostrar:
+
     cout<<"Polinomio 1: ";
-    for(int i=0;i<3;i++){
-        cout<<"+("<<coef1[i]<<")x^"<<3-i-1;
+    for(int i=0;i<=g1;i++){
+        if(coef1[i] != 0){
+            if(i>0   && coef1[i]>0){
+                cout<<" + ";
+            }
+
+            cout<<coef1[i]<<"x^"<<g1-i;
+        }
+
     }
 
-    cout<<"\nPolinomio 2: ";
-    for(int i=0;i<2;i++){
-        cout<<"+("<<coef2[i]<<")x^"<<2-i-1;
+    cout << "\nPolinomio 2: ";
+    for(int i=0;i<=g2;i++){
+        if(coef2[i] != 0){
+            if(i>0   && coef2[i]>0){
+                cout<<" + ";
+            }
+
+            cout<<coef2[i]<<"x^"<<g2-i;
+        }
+    }
+    cout << "\nPolinomio 3: ";
+    for(int i=0;i<=g3;i++){
+        if(coef3[i] != 0){
+            if(i>0   && coef3[i]>0){
+                cout << " + ";
+            }
+
+            cout<<coef3[i]<<"x^"<<g3-i;
+        }
     }
 
-    cout<<"\nPolinomio 3: ";
-    for(int i=0;i<3;i++){
-        cout<<"+("<<coef3[i]<<")x^"<<3-i-1;
-    }
+    cout<<"\n\n";
+    sumarPolinomios(coef1, coef2, coef3, g1, g2, g3);
+    multiplicarPolinomios(coef1, coef2, coef3, g1, g2, g3);
 
-    cout<<"\nSuma de los tres polinomios: ";
-    sumarPolinomios(coef1,coef2, coef3,  g1,  g2,  g3);
-
-
-
-
-
-
-
-
-
-    cout<<"\n";
     delete[] coef1;
     delete[] coef2;
     delete[] coef3;
